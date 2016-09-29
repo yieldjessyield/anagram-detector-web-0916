3f894182aa2ae118bdecd3fe04d2cf92a89704e3
Jessica Harrelson <jess.harrelson0707@gmail.com>
2016-09-29 15:06:08 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
+
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matched_words = []
+    sorted_word = @word.chars.sort.join
+    array.map do |potential_word|
+      sorted_potential_word = potential_word.chars.sort.join
+      if sorted_potential_word == sorted_word
+        matched_words << potential_word
+      end
+    end
+    matched_words
+  end
+end
+
